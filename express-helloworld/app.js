f866489f69360945b318632fd7851f9811542f27
parvthebest <44346438+parvthebest@users.noreply.github.com>
2020-04-25 18:57:24 +0530

    updated app.js

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,10 @@ app.get('/mars', function (req, res) {
   res.send('Hello Mars!\n');
 });
 
+app.get('/parv', function (req, res) {
+  res.send('Hello Parv!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
